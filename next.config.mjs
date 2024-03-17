ce2fa5e1e5774a0459310989758d42e4c94a536a
dolapo oluwole <adefisayomioluwole@gmail.com>
2024-03-17 22:58:52 +0100

    add environmental variable to the appropriate files

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,9 @@ const nextConfig = {
     FLUTTERWAVE_PUBLIC_KEY: "FLWPUBK_TEST-c039b1ba2ecc868a1eaff486681bbd5f-X",
     FLUTTERWAVE_SECRET_KEY: "FLWSECK_TEST-ce968e5a6f4556ab552dd3368b64f9a4-X",
     FLUTTERWAVE_ENCRYPTION_KEY: "FLWSECK_TEST7b79ab345091",
-    firebaseConfig: {
-      apiKey: "AIzaSyCTdrX32ASEeEo4goCYD_SYepK_PvZiGPw",
-      authDomain: "clace-fa078.firebaseapp.com",
-      projectId: "clace-fa078",
-      storageBucket: "clace-fa078.appspot.com",
-      messagingSenderId: "323117669192",
-      appId: "1:323117669192:web:24addff32125356988f17d",
-      measurementId: "G-0VMR8LPZ3X"
-    },
+    FIREBASE_API_KEY: "AIzaSyCTdrX32ASEeEo4goCYD_SYepK_PvZiGPw",
+    FIREBASE_APP_ID: "1:323117669192:web:24addff32125356988f17d",
+    FIREBASE_MEASUREMENT_ID: "G-0VMR8LPZ3X"
   },
   images: {
     domains: ['cdn.sanity.io'],
--- src/constants.ts
+++ src/constants.ts
@@ -15,4 +15,12 @@ export const errorMessage = (message: string) => {
 }
 
 export const FLUTTERWAVE_PUBLIC_KEY= process.env.FLUTTERWAVE_PUBLIC_KEY!
-export const firebaseConfig = process.env.firebaseConfig
+export const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "clace-fa078.firebaseapp.com",
+    projectId: "clace-fa078",
+    storageBucket: "clace-fa078.appspot.com",
+    messagingSenderId: "323117669192",
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  }
