ed2bb109702db3175aa0e55cb825fa815712360b
dolapo oluwole <adefisayomioluwole@gmail.com>
2024-03-18 00:08:45 +0100

    added styling to the user menu image

--- src/layout/header/userMenu/userMenu.tsx
+++ src/layout/header/userMenu/userMenu.tsx
@@ -36,8 +36,8 @@ import { useRouter } from "next/router"
         ) : (
           <DropdownMenu>
             <DropdownMenuTrigger asChild className="cursor-pointer">
-              <Avatar className=" border-2 border-muted rounded-full w-9 h-9 ">
-                <AvatarImage src={user?.photoURL!} alt={user?.displayName!} />
+              <Avatar className=" border-2 border-muted rounded-full w-10 h-10 p-[2px]">
+                <AvatarImage src={user?.photoURL!} alt={user?.displayName!} className="w-10 h-10 object-cover rounded-full border-2 p-[2px]" />
                 <AvatarFallback className="bg-background text-lg uppercase" >{user?.displayName?.slice(0, 2)}</AvatarFallback>
               </Avatar>
             </DropdownMenuTrigger>
